ea54ed44a3961fe95e0ee6f26d29efaa4a18c555
NVATMS04M14I628S <NVATMS04M14I628S@SISTEMI_23.didatticariva.local>
2023-02-09 12:18:10 +0100

    primo commit

---
+++ project_thomas/src/main/java/mai/Github.java
@@ -0,0 +1,10 @@
+package mai;
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ project_thomas/src/main/java/mai/MAin.java
@@ -0,0 +1,5 @@
+package mai;
+
+public class MAin {
+
+}
